4070657e02906cfee02d7961109393333cd93d6a
unknown <goldenleon5632@gmail.com>
2019-01-11 15:04:12 +0700

    'V0.5.1'

--- resources/views/partials/jumbotron-content/search-jumbotron.blade.php
+++ resources/views/partials/jumbotron-content/search-jumbotron.blade.php
@@ -1,5 +1,9 @@
 @extends('partials.jumbotron')
 @section('jumbotron')
+    <h1 class="heading-primary">
+        <span class="heading-primary--main">Boarding Life</span>
+        <span class="heading-primary--sub">Ease your life</span>
+    </h1>
     <div class="search">
         <input type="text" class="searchTerm" placeholder="What are you looking for?">
         <button type="submit" class="searchButton">
