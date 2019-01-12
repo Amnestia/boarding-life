7b881a6752ce281c7396f82ce013f256f9305da9
Amnestia <goldenleon5632@gmail.com>
2019-01-12 08:33:42 +0700

    v1.1 fixed css

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -37,7 +37,7 @@
             integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
             crossorigin="anonymous">
     </script>
-    <script src="{{URL::asset('js/map.js')}}"></script>
-    <script src="{{URL::asset('js/script.js')}}"></script>
+    <script src="{{asset('js/map.js')}}"></script>
+    <script src="{{asset('js/script.js')}}"></script>
     </body>
 </html>
